28e981b502547d2731843a64612bf64f3bf3c322
KouyaFukuda <kouyaf77@gmail.com>
2013-10-09 22:55:45 +0900

    Created function of login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,15 @@ class ApplicationController < ActionController::Base
 
   rescue_from AbstractController::ActionNotFound, with: :error_404
   rescue_from SlideShare::ServiceError, with: :error_404
-  
+
+  def after_sign_in_path_for resource
+    user_path resource
+  end
   private
   def error_404
     respond_to do |format|
       format.html { render :error_404, layout: 'application', status: 404 }
       format.all { render nothing: true, status: 404 }
-    end 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 S2::Application.routes.draw do
+  root "home#index"
+
+  devise_for :users
+
   controller :streaming do
     get "streaming/:id" => :index, as: :index
   end
+
+  resources :users, only: [:show]
 end
