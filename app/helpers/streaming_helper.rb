b71cfafe5dd288b44c65f63be1f5d6c58a666e39
dxhuy1988 <doxuanhuy@gmail.com>
2013-10-11 00:04:02 +0900

    refactoring a bit

--- app/helpers/streaming_helper.rb
+++ app/helpers/streaming_helper.rb
@@ -6,4 +6,16 @@ module StreamingHelper
   def first_page(slide)
     "http://#{slide[:image_info][:prefix]}1#{slide[:image_info][:suffix]}"
   end
+
+  def pages(slide)
+    slide[:image_info][:total_slides]
+  end
+
+  def suffix(slide)
+    slide[:image_info][:suffix]
+  end
+
+  def prefix(slide)
+    slide[:image_info][:prefix]
+  end
 end
