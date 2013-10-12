50d8a14cc7f0a2bdb94b97d5c95fc244a2008628
KouyaFukuda <kouyaf77@gmail.com>
2013-10-13 01:33:42 +0900

    Create Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    slide_share = SlideShare::Base.new(
+       api_key: ENV['API_KEY'],
+       shared_secret: ENV['SHARED_SECRET'])
+    slide_share_obj = slide_share.slideshows.find_all_by_user current_user.username
+    @slides = case slide_share_obj["User"]["Count"]
+      when "0"
+        []
+      when "1"
+        [slide_share_obj["User"]["Slideshow"]]
+      else
+        slide_share_obj["User"]["Slideshow"]
+      end
   end
 end
