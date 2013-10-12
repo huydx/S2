1905f3f111099e695ff8e9dc5c64bc29bc7528ee
KouyaFukuda <kouyaf77@gmail.com>
2013-10-12 23:49:36 +0900

    Added line breake

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
    build_resource(sign_up_params)
+
    begin
      slide_share = SlideShare::Base.new(
        api_key: ENV['API_KEY'],
        shared_secret: ENV['SHARED_SECRET'])
      slide_share.leads.find_campaigns_by_user("#{sign_up_params["username"]}","#{sign_up_params["password"]}")
+
      if resource.save!
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
@@ -19,6 +21,7 @@ class RegistrationsController < Devise::RegistrationsController
         if is_navigational_format?
           set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
         end
+
         expire_session_data_after_sign_in!
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
