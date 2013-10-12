212333d7fe2194f2212f84b69891e927fc54830f
KouyaFukuda <kouyaf77@gmail.com>
2013-10-12 22:56:10 +0900

    Changed email to username

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
-  def create
+  def new
     super
+  end
+
+  def create
+   build_resource(sign_up_params)
+   begin
+     SlideShare::Base.new(api_key: ENV['API_KEY'], shared_secret: ENV['SHARED_SECRET']).leads.find_campaigns_by_user("#{sign_up_params["username"]}",  "#{sign_up_params["password"]}")
+     if resource.save!
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+     end
+   rescue
+     build_resource(sign_up_params)
+     clean_up_passwords resource
+     render action: :new
+   end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
