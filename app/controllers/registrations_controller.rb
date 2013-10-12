71512cb07d29a28315e871803e8251a25816f38d
KouyaFukuda <kouyaf77@gmail.com>
2013-10-12 23:30:09 +0900

    Fixed code commented

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,16 +6,22 @@ class RegistrationsController < Devise::RegistrationsController
   def create
    build_resource(sign_up_params)
    begin
-     SlideShare::Base.new(api_key: ENV['API_KEY'], shared_secret: ENV['SHARED_SECRET']).leads.find_campaigns_by_user("#{sign_up_params["username"]}",  "#{sign_up_params["password"]}")
+     SlideShare::Base.new(
+       api_key: ENV['API_KEY'],
+       shared_secret: ENV['SHARED_SECRET']
+     ).leads.find_campaigns_by_user("#{sign_up_params["username"]}",
+       "#{sign_up_params["password"]}")
      if resource.save!
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        if is_navigational_format?
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
+        end
         expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
      end
    rescue
