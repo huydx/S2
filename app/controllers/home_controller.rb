22616714219412ed3eaac7fb14fb814786947137
KouyaFukuda <kouyaf77@gmail.com>
2013-10-13 02:17:18 +0900

    Fixed code commented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,14 @@ class HomeController < ApplicationController
        api_key: ENV['API_KEY'],
        shared_secret: ENV['SHARED_SECRET'])
     slide_share_obj = slide_share.slideshows.find_all_by_user current_user.username
+
     @slides = case slide_share_obj["User"]["Count"]
-      when "0"
-        []
-      when "1"
-        [slide_share_obj["User"]["Slideshow"]]
-      else
-        slide_share_obj["User"]["Slideshow"]
-      end
+              when "0"
+                []
+              when "1"
+                [slide_share_obj["User"]["Slideshow"]]
+              else
+                slide_share_obj["User"]["Slideshow"]
+              end
   end
 end
