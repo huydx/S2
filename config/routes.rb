e6ec4bdc21caed014a23476eda9adc9256397a3c
KouyaFukuda <kouyaf77@gmail.com>
2013-10-13 16:44:56 +0900

    Deleted sing_up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 S2::Application.routes.draw do
   root "top#index"
 
-  devise_for :users, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: {sessions: "sessions"}
 
   controller :streaming do
     get "streaming/:id" => :index, as: :index
     get "streaming/search/:username" => :search
     post "streaming/register" => :register_channel
-  end 
+  end
 
   resources :users, only: [:show]
   resources :home, only: [:index]
