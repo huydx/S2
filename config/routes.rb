8746dc24f5bf7f83f68bde2ad161ee6d5670e97b
KouyaFukuda <kouyaf77@gmail.com>
2013-10-31 01:13:56 +0900

    Fixed bug for confilct

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,10 @@ S2::Application.routes.draw do
   controller :streaming do
     get "streaming/search" => :search
     get "streaming/:id" => :index, as: :index
-    get "streaming/:id/sub" => :index
+    get "streaming/:id/client" => :index
     post "streaming/register" => :register_channel
     post "streaming/remove" => :remove_channel
-<<<<<<< HEAD
-    get "streaming/:id/client" => :client
-  end 
-=======
   end
->>>>>>> Created url for subscribe
 
   resources :users, only: [:show]
   resources :home, only: [:index]
